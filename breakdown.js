dfae63481236f5b19e6292b74c2441008cf908a7
Michael Seth <michael.a.seth175@gmail.com>
2021-02-26 11:32:37 -0500

    made a for looop to grab each passenger info, create it into a new passenger, and reassign to another array called manifest

--- breakdown.js
+++ breakdown.js
@@ -9,7 +9,7 @@ let arrayOfAttributes = [];
 for ( let i = 0; i < arrayOfStrings.length ; i++) {
     arrayOfAttributes[i] = arrayOfStrings[i].split(',');
 };
-    // console.table(arrayOfAttributes);
+    console.table(arrayOfAttributes);
 
 //Build a class
 
@@ -19,22 +19,37 @@ class Passenger {
     constructor (survived, pclass, name, sex, age, siblingsSpouses , parentsChildren, fare) {
         this.survived = survived;
         this.pclass = pclass;
-        this.name =  this.parseName(name);
+        // this.name =  //
+        this.parseName(name);
         this.sex = sex;
         this.age = age;
         this.siblingsSpouses = siblingsSpouses;
-        this.parentsChildren - parentsChildren;
+        this.parentsChildren = parentsChildren;
         this.fare =  fare;
 
     }
     parseName (name){
-        this.name = name.split(" ", 3);
-        this.title = this.name[0];
-        return this.name;
+        let nameArray = name.split(" ", 3);
+        this.name = {
+            title: nameArray[0],
+            fname: nameArray[1],
+            lname: nameArray[2]
+        }
+        // return this.name;
     }
 
 }
 
-let bob = new Passenger(1, 3, "Mr. Bob Hardy", "male", 22, 1, 0, 7.25);
+// Survived,Pclass,Name,Sex,Age,Siblings/Spouses Aboard,Parents/Children Aboard,Fare
+let manifest = [];
 
-console.log(bob);
+for ( let i = 0; i < arrayOfAttributes.length; i++){
+    manifest[i] = new Passenger(arrayOfAttributes[i][0], arrayOfAttributes[i][1], arrayOfAttributes[i][2], arrayOfAttributes[i][3], arrayOfAttributes[i][4], arrayOfAttributes[i][5] , arrayOfAttributes[i][6], arrayOfAttributes[i][7])
+    // manifest[i] = new Passenger(arrayOfAttributes[0]);
+}
+
+console.log(manifest);
+
+// let bob = new Passenger(1, 3, "Mr. Bob Hardy", "male", 22, 1, 0, 7.25);
+
+// console.log(bob);
